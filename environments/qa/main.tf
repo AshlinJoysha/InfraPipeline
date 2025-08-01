ba66bef2267c6b027a5d4e21fac767ad47e03627
Ashlin <you@example.com>
2025-08-01 15:09:31 +0530

    added qa configs

---
+++ environments/qa/main.tf
@@ -0,0 +1,16 @@
+module "vpc"{
+    source = "../../modules/vpc"
+    vpc_cidr = "10.0.0.0/16"
+    env = "qa"
+    mysubnet_cidr = "10.0.1.0/24" 
+    subnetaz = "ap-southeast-1a"
+}
+
+module "ec2"{
+    source = "../../modules/ec2"
+    ami = "ami-02c7683e4ca3ebf58"
+    instancetype = "t2.medium"
+    subnetid = module.vpc.mysubnetid
+    count = 1
+    env = "qa"
+}
