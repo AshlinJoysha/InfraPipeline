cc9cc079c9941d020f318d26f1b0059a217e8867
AshlinJoysha <ashlinjoysha@gmail.com>
2025-08-04 14:36:34 +0530

    Update count of variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -10,6 +10,9 @@ variable "subnetid"{
     type = string 
     description = "Our preferred subnet where we need to launch our machine"
 }
+variable "count"{
+    type = number 
+    description = "Our preferred instance type"
 
 variable "env"{
     type = string 
